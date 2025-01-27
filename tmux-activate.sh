a61374020e02641a6ae0be639f7627597e72d53c
novka-serv <novatrace@yandex.ru>
2025-01-27 10:47:20 +0800

    27.01-10.47

---
+++ tmux-activate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+git config advice.addIgnoredFile false
+git config --global user.email "novatrace@yandex.ru"
+git config --global user.name "novka-serv"
+
+session="minecraft"
+
+if tmux has-session -t "$session" 2>/dev/null; then
+    echo "session $session already exists"
+    return 0
+fi
+
+tmux new-session -d -s $session
+tmux split-window -h
+tmux send-keys -t $session:0.1 "btop" C-m
+tmux select-pane -t $session:0.0
+
+tmux attach -t $session
